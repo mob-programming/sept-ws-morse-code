2af572b0266304560b4be30e5349bab50f079363
mob-programmer <48465586+mob-programmer@users.noreply.github.com>
2019-09-10 19:50:35 -0700

    created test a to .-

--- test/morseCodeTest.js
+++ test/morseCodeTest.js
@@ -4,6 +4,10 @@ describe( "morseCodeTranslator", () => {
     it("Should give back a .- when given an a", () => {
         assert.equal(morseCodeTranslator("a"), ".-");
     });
+
+    it("Should give back an a when given .-", () => {
+        assert.equal(morseCodeTranslator(".-"), "a");
+    });
 });
 
 
