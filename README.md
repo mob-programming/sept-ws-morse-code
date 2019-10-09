935e168080c839dd1d2f1f498bc59304e74479af
mob-programmer <48465586+mob-programmer@users.noreply.github.com>
2019-10-08 19:07:56 -0700

    added morse code to english test

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Code from the September mob programming workshop
 ## Programming problem
 
 Write a program that automatically converts English text to Morse code and vice versa.
+
+## Future ideas:
+- Add sound.
--- test/morseCodeTest.js
+++ test/morseCodeTest.js
@@ -2,18 +2,23 @@ const assert = require('assert');
 
 describe( "morseCodeTranslator", () => {
     it("Should give back a morse code character when given a letter", () => {
-        assert.equal(morseCodeTranslator("a"), ".-");
-        assert.equal(morseCodeTranslator("b"), "-...");
+        assert.equal(englishLetterToMorseCode("a"), ".-");
+        assert.equal(englishLetterToMorseCode("b"), "-...");
+        assert.equal(englishLetterToMorseCode("c"), "-.-.");
     });
 
     it("Should give back an a when given .-", () => {
-        assert.equal(morseCodeTranslator(".-"), "a");
+        assert.equal(morseCodeToEnglishLetter(".-"), "a");
     });
 });
 
 
-function morseCodeTranslator(foo){
-    if(foo === ".-") return "a";
+function englishLetterToMorseCode(foo){
     if(foo === "b") return "-...";
+    if(foo === "c") return "-.-.";
     return ".-";
+};
+
+function morseCodeToEnglishLetter(foo){
+    if(foo === ".-") return "a";
 };
