202f094e1e29fe3378476be0ea4b40fd312b0cf4
mob-programmer <48465586+mob-programmer@users.noreply.github.com>
2019-10-08 20:02:37 -0700

    refactored with dictionary

--- test/morseCodeTest.js
+++ test/morseCodeTest.js
@@ -14,11 +14,48 @@ describe( "morseCodeTranslator", () => {
 
 
 function englishLetterToMorseCode(foo){
-    if(foo === "b") return "-...";
-    if(foo === "c") return "-.-.";
-    return ".-";
+    return englishToMorseCodeDictionary[foo]
 };
 
 function morseCodeToEnglishLetter(foo){
-    if(foo === ".-") return "a";
+    return Object.keys(englishToMorseCodeDictionary).find(key => englishToMorseCodeDictionary[key] === foo);
 };
+
+var englishToMorseCodeDictionary = {
+"a": ".-",
+"b": "-...",
+"c": "-.-.",
+"d": "-..",
+"e": ".",
+"f": "..-.",
+"g": "--.",
+"h": "....",
+"i": "..",
+"j": ".---",
+"k": "-.-",
+"l": ".-..",
+"m": "--",
+"n": "-.",
+"o": "---",
+"p": ".--.",
+"q": "--.-",
+"r": ".-.",
+"s": "...",
+"t": "-",
+"u": "..-",
+"v": "...-",
+"w": ".--",
+"x": "-..-",
+"y": "-.--",
+"z": "--..",
+"1": ".----",
+"2": "..---",
+"3": "...--",
+"4": "....-",
+"5": ".....",
+"6": "-....",
+"7": "--...",
+"8": "---..",
+"9": "----.",
+"0": "-----",
+}
